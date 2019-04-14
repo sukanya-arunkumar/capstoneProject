55e312f8eb513c770d1abef2f31b4645b81a8e98
Celina Khalife <celina.khalife@gmail.com>
2019-04-14 12:33:54 +0300

    fix: trips query condition to remove all missing fields

--- python/requests_package.py
+++ python/requests_package.py
@@ -1,7 +1,7 @@
 from google.cloud import bigquery
 import csv
 
-def query_trips(type, postFix = ""):
+def query_trips(type, postFix, year):
     client = bigquery.Client()
     # Set use_legacy_sql to True to use legacy SQL syntax.
     job_config = bigquery.QueryJobConfig()
@@ -9,15 +9,18 @@ def query_trips(type, postFix = ""):
 
     query_job = client.query("""
     SELECT *
-    FROM [nyc-tlc:""" + type + """.trips""" + postFix + """]
-    WHERE ABS(FARM_FINGERPRINT(CONCAT(STRING(pickup_longitude), STRING(pickup_latitude), STRING(pickup_datetime), STRING( dropoff_datetime)))) % 10 < 8
+    FROM
+    (SELECT * FROM
+     [nyc-tlc:""" + type + """.trips""" + postFix + """] WHERE YEAR( pickup_datetime ) = """ + year + """ AND pickup_latitude <> 0 AND pickup_longitude <> 0 AND dropoff_latitude <> 0 AND dropoff_longitude <> 0 AND trip_distance > 0 AND fare_amount > 0)
+    WHERE ABS(FARM_FINGERPRINT(STRING(pickup_datetime))) % 10 < 8
     LIMIT 50000
     """,
     job_config = job_config)
 
     results = query_job.result().to_dataframe()  # Waits for job to complete.
-    results.to_csv(type + postFix + '.csv', encoding='utf-8')
+    results.to_csv(type + '_' + year + '.csv', encoding='utf-8')
 
-query_trips('yellow')
-query_trips('green', '_2014')
-query_trips('green', '_2015')
+query_trips('yellow','', '2014')
+query_trips('yellow','', '2015')
+query_trips('green','_2014', '2014')
+query_trips('green','_2015', '2015')
