b605fc60c0cde4eb32895a3784c558751cc4771c
Celina Khalife <celina.khalife@gmail.com>
2019-04-20 21:04:45 +0300

    feat: create multiple columns for datetime fields

--- clean/data.R
+++ clean/data.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(ggmap)
 library(geosphere)
 
-register_google(key=[key])
+register_google(key="AIzaSyDOHYjR93Vi0ols4DpE88pdPOppaO_aShg")
 
 
 #load yellow and green datasets
@@ -14,7 +14,7 @@ yellowData2015 <- read.csv('./yellow_2015.csv', stringsAsFactors = FALSE)
 greenData2014 <- read.csv('./green_2014.csv', stringsAsFactors = FALSE)
 greenData2015 <- read.csv('./green_2015.csv', stringsAsFactors = FALSE)
 
-visualizeAndCleanPickupLatLngData <- function (data) {
+visualizeAndCleanPickupLatLngData <- function (data, drawPlots = FALSE) {
   
   # set ggplot limits
   min_lat <- min(data$pickup_latitude)
@@ -22,12 +22,14 @@ visualizeAndCleanPickupLatLngData <- function (data) {
   min_long <- min(data$pickup_longitude)
   max_long <- max(data$pickup_longitude)
   
-  plot <- ggplot(data, aes(x=pickup_longitude, y=pickup_latitude)) +
-    geom_point(size=0.06) +
-    scale_x_continuous(limits=c(min_long, max_long)) +
-    scale_y_continuous(limits=c(min_lat, max_lat)) 
-  
-  plot
+  if(drawPlots){
+    plot <- ggplot(data, aes(x=pickup_longitude, y=pickup_latitude)) +
+      geom_point(size=0.06) +
+      scale_x_continuous(limits=c(min_long, max_long)) +
+      scale_y_continuous(limits=c(min_lat, max_lat)) 
+    
+    plot
+  }
   
   data <- data[data$pickup_latitude >= -85 & data$pickup_latitude <= 85,]
   data <- data[data$pickup_longitude >= -180 & data$pickup_longitude <= 180,]
@@ -51,29 +53,41 @@ visualizeAndCleanPickupLatLngData <- function (data) {
   min_long <- min(data$pickup_longitude)
   max_long <- max(data$pickup_longitude)
   
-  plot <- ggplot(data, aes(x=pickup_longitude, y=pickup_latitude)) +
-    geom_point(size=0.06) +
-    scale_x_continuous(limits=c(min_long, max_long)) +
-    scale_y_continuous(limits=c(min_lat, max_lat)) 
+  if(drawPlots){
+    
+    plot <- ggplot(data, aes(x=pickup_longitude, y=pickup_latitude)) +
+      geom_point(size=0.06) +
+      scale_x_continuous(limits=c(min_long, max_long)) +
+      scale_y_continuous(limits=c(min_lat, max_lat)) 
+    
+    plot
+    
+    # draw pickup data on google maps
+    ggmap(get_map("New York",
+                  zoom = 10, scale = "auto",
+                  source = "google"),
+          extent="device",
+          legend="topright"
+    ) + geom_point(aes(x=pickup_longitude, y=pickup_latitude), 
+                   data=data, 
+                   col="red", alpha=0.2
+    ) 
+  }
   
-  plot
-  
-  # draw pickup data on google maps
-  ggmap(get_map("New York",
-                zoom = 10, scale = "auto",
-                source = "google"),
-        extent="device",
-        legend="topright"
-  ) + geom_point(aes(x=pickup_longitude, y=pickup_latitude), 
-                 data=data, 
-                 col="red", alpha=0.2
+  data <- data %>% mutate(
+    pickup_year = mapply(function(datetime) as.POSIXlt(datetime)$year+1900 , pickup_datetime),
+    pickup_month = mapply(function(datetime) as.POSIXlt(datetime)$mon+1 , pickup_datetime),
+    pickup_day = mapply(function(datetime) as.POSIXlt(datetime)$mday , pickup_datetime),
+    pickup_hour = mapply(function(datetime) as.POSIXlt(datetime)$hour , pickup_datetime),
+    pickup_minute = mapply(function(datetime) as.POSIXlt(datetime)$min , pickup_datetime)
   ) 
+  
   data
 }
 
 
 
-visualizeAndCleanDropoffLatLngData <- function (data) {
+visualizeAndCleanDropoffLatLngData <- function (data, drawPlots = FALSE) {
   
   # set ggplot limits
   min_lat <- min(data$dropoff_latitude)
@@ -81,12 +95,14 @@ visualizeAndCleanDropoffLatLngData <- function (data) {
   min_long <- min(data$dropoff_longitude)
   max_long <- max(data$dropoff_longitude)
   
-  plot <- ggplot(data, aes(x=dropoff_longitude, y=dropoff_latitude)) +
-    geom_point(size=0.06) +
-    scale_x_continuous(limits=c(min_long, max_long)) +
-    scale_y_continuous(limits=c(min_lat, max_lat)) 
-  
-  plot
+  if(drawPlots){
+    plot <- ggplot(data, aes(x=dropoff_longitude, y=dropoff_latitude)) +
+      geom_point(size=0.06) +
+      scale_x_continuous(limits=c(min_long, max_long)) +
+      scale_y_continuous(limits=c(min_lat, max_lat)) 
+    
+    plot
+  }
   
   data <- data[data$dropoff_latitude >= -85 & data$dropoff_latitude <= 85,]
   data <- data[data$dropoff_longitude >= -180 & data$dropoff_longitude <= 180,]
@@ -110,28 +126,37 @@ visualizeAndCleanDropoffLatLngData <- function (data) {
   min_long <- min(data$dropoff_longitude)
   max_long <- max(data$dropoff_longitude)
   
-  plot <- ggplot(data, aes(x=dropoff_longitude, y=dropoff_latitude)) +
-    geom_point(size=0.06) +
-    scale_x_continuous(limits=c(min_long, max_long)) +
-    scale_y_continuous(limits=c(min_lat, max_lat)) 
+  if(drawPlots){
+    plot <- ggplot(data, aes(x=dropoff_longitude, y=dropoff_latitude)) +
+      geom_point(size=0.06) +
+      scale_x_continuous(limits=c(min_long, max_long)) +
+      scale_y_continuous(limits=c(min_lat, max_lat)) 
+    
+    plot
+    
+    # draw pickup data on google maps
+    ggmap(get_map("New York",
+                  zoom = 10, scale = "auto",
+                  source = "google"),
+          extent="device",
+          legend="topright"
+    ) + geom_point(aes(x=dropoff_longitude, y=dropoff_latitude), 
+                   data=data, 
+                   col="red", alpha=0.2
+    ) 
+  }
   
-  plot
-  
-  # draw pickup data on google maps
-  ggmap(get_map("New York",
-                zoom = 10, scale = "auto",
-                source = "google"),
-        extent="device",
-        legend="topright"
-  ) + geom_point(aes(x=dropoff_longitude, y=dropoff_latitude), 
-                 data=data, 
-                 col="red", alpha=0.2
-  ) 
-  
+  data <- data %>% mutate(
+    dropoff_year = mapply(function(datetime) as.POSIXlt(datetime)$year+1900 , dropoff_datetime),
+    dropoff_month = mapply(function(datetime) as.POSIXlt(datetime)$mon+1 , dropoff_datetime),
+    dropoff_day = mapply(function(datetime) as.POSIXlt(datetime)$mday , dropoff_datetime),
+    dropoff_hour = mapply(function(datetime) as.POSIXlt(datetime)$hour , dropoff_datetime),
+    dropoff_minute = mapply(function(datetime) as.POSIXlt(datetime)$min , dropoff_datetime)
+    
+    )
+
   data
 }
-
-
 
 yellowData2014 <- visualizeAndCleanPickupLatLngData(yellowData2014)
 yellowData2015 <- visualizeAndCleanPickupLatLngData(yellowData2015)
@@ -149,4 +174,3 @@ write.csv(yellowData2014, file = "clean_yellow_2014.csv")
 write.csv(yellowData2015, file = "clean_yellow_2015.csv")
 write.csv(greenData2014, file = "clean_green_2014.csv")
 write.csv(greenData2015, file = "clean_green_2015.csv")
-
